1388124954822a32a133749547f25a3a2cb7f4af
dannyhu1 <nikolas11@poczta.fm>
2023-10-19 11:47:14 +0200

    1.3

--- part_1/1.3/src/App.jsx
+++ part_1/1.3/src/App.jsx
@@ -32,12 +32,6 @@ const Total = (props) => {
   )
 }
 
-
-
-
-
-
-
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = {
@@ -55,7 +49,14 @@ const App = () => {
 
   return (
     <div>
-      ...
+      <Header course = {course}/>
+      <Content 
+      part1 = {part1.name} exercises1 = {part1.exercises}
+      part2 = {part2.name} exercises2 = {part2.exercises}
+      part3 = {part3.name} exercises3 = {part3.exercises}
+      />
+      <Total  exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}   />
+
     </div>
   )
 }
